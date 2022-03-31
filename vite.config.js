d13f47bceeae4277c401676bf1a27bff07be2de0
naveennamani <naveennamani5@gmail.com>
2022-03-31 20:11:42 +0530

    setup @mdx-js
    * install @mdx-js/rollup for vite plugin
    * install @mdx-js/react for component provider
    * add "type":"module" in package.json
    * rename server/index.js to server/index.cjs
    * change scripts in package.json

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
+import mdx from "@mdx-js/rollup";
 
-export default {
-  plugins: [react(), ssr()],
-}
+export default defineConfig({
+  plugins: [
+    react(),
+    mdx({
+      providerImportSource: "@mdx-js/react",
+    }),
+    ssr(),
+  ],
+});
